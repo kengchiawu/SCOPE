fac650d8ec80498a3743651f6e15d5ab0f62813e
199u <1161057855@qq.com>
2025-01-06 16:24:07 +0800

    添加sh文件中的初始参数，改变data文件位置，修改包版本

--- eval_test.sh
+++ eval_test.sh
@@ -1,15 +1,16 @@
-export CUDA_VISIBLE_DEVICES=$1
+export CUDA_VISIBLE_DEVICES=4,5
 
-method=$2 # Support ALLKV, PyramidKV, SnapKV, H2O, StreamingLLM
-max_capacity_prompts=$3
-attn_implementation=$4 # Support "flash_attention_2", "sdpa", "eager".
-source_path=$5
-model_path=$6
-decoding_metric=$7 # H2O Support None,h2o,(fixed,linear,jump)---SCOPE
-decoding_window_size=$8
-save_dir=$9 # path to result save_dir
-K=$10 #30,60
-T=$11
+method=H2O # Support ALLKV, PyramidKV, SnapKV, H2O, StreamingLLM
+max_capacity_prompts=512
+attn_implementation=flash_attention_2 # Support "flash_attention_2", "sdpa", "eager".
+#source_path=$5
+model_path='meta-llama/Llama-3.2-1B-Instruct'
+decoding_metric=h2o # H2O Support None,h2o,(fixed,linear,jump)---SCOPE
+decoding_window_size=64
+decoding_recent_size=64
+save_dir='~/SCOPE/results' # path to result save_dir
+K=30 #30,60
+T=20
 
 python3 run_longgenbench.py \
     --method ${method} \
