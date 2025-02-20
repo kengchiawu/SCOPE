04182c23e2c0fe6893ddab485cecb557d70da039
199u <1161057855@qq.com>
2025-02-20 16:09:23 +0800

    sh遍历decoding_metric方法

--- eval_gen.sh
+++ eval_gen.sh
@@ -1,4 +1,8 @@
 results_dir="./results/llama-3.1-8b-instruct_2048_eager"
-
-python3 eval_longgenbench.py \
-    --results_dir ${results_dir}
+decoding_metric="jump"
+for decoding_metric in None fixed linear jump
+do
+    python3 eval_longgenbench.py \
+        --results_dir ${results_dir} \
+        --decoding_metric ${decoding_metric} 
+done
