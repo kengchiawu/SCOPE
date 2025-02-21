63776bbb285ef9c1abd3de38b74231a08334f8fa
199u <1161057855@qq.com>
2025-02-21 23:00:03 +0800

    add code in line 931&940 to save key_cache

--- model/llama_model.py
+++ model/llama_model.py
@@ -13,7 +13,7 @@ from transformers.modeling_outputs import BaseModelOutputWithPast
 from transformers.utils import (
     logging,
 )
-from model.kv_utils import init_pyramidkv,init_snapkv,init_H2O,init_StreamingLLM,init_ALLKV,init_Quest
+from model.kv_utils import init_pyramidkv,init_snapkv,init_H2O,init_StreamingLLM,init_ALLKV
 import math
 from flash_attn import flash_attn_func, flash_attn_varlen_func
 from flash_attn.bert_padding import index_first_axis, pad_input, unpad_input
@@ -860,7 +860,7 @@ def llama_attn_forward_ALLKV(
 ) -> Tuple[torch.Tensor, Optional[torch.Tensor], Optional[Tuple[torch.Tensor]]]:
     bsz, q_len, _ = hidden_states.size()
 
-    init_ALLKV(self, num_hidden_layers=self.config.num_hidden_layers)
+    init_ALLKV(self)
 
     if self.config.pretraining_tp > 1:
         key_value_slicing = (self.num_key_value_heads * self.head_dim) // self.config.pretraining_tp
@@ -928,6 +928,7 @@ def llama_attn_forward_ALLKV(
             self.kv_seq_len = kv_seq_len
             key_states_compress, value_states_compress = self.kv_cluster.update_kv(key_states, query_states, value_states, attention_mask, self.num_key_value_groups)
             past_key_value.update(key_states_compress, value_states_compress, self.layer_idx, cache_kwargs)
+            torch.save(past_key_value.key_cache[self.layer_idx],f'./kv_cache/prefill/layer_{self.layer_idx}')
         else:
             self.kv_seq_len += q_len
             key_states, value_states = past_key_value.update(key_states, value_states, self.layer_idx, cache_kwargs)
@@ -936,6 +937,9 @@ def llama_attn_forward_ALLKV(
             key_states_compress, value_states_compress = self.kv_cluster.update_kv_in_decoding(history_key_states, query_states, history_value_states, attention_mask, self.num_key_value_groups)
             past_key_value.key_cache[self.layer_idx] = key_states_compress
             past_key_value.value_cache[self.layer_idx] = value_states_compress
+            if past_key_value.key_cache[self.layer_idx].shape[-2] == 3000:
+                torch.save(past_key_value.key_cache[self.layer_idx],f'./kv_cache/decode/layer_{self.layer_idx}')
+            
             # torch.cuda.empty_cache()
             # print(f"Original value_states size: {value_states.size()}")
             # print(f"Compressed value_states size: {value_states_compress.size()}")
@@ -1004,7 +1008,7 @@ def llama_sdpa_attn_forward_ALLKV(
             position_embeddings=position_embeddings, # Add to fix 4.44.2
         )
 
-    init_ALLKV(self, num_hidden_layers=self.config.num_hidden_layers)
+    init_ALLKV(self)
 
     bsz, q_len, _ = hidden_states.size()
 
@@ -1108,7 +1112,7 @@ def llama_flash_attn2_forward_ALLKV(
     **kwargs,
 ) -> Tuple[torch.Tensor, Optional[torch.Tensor], Optional[Tuple[torch.Tensor]]]:
     # [SnapKV] register kv_cluster
-    init_ALLKV(self, num_hidden_layers=self.config.num_hidden_layers)
+    init_ALLKV(self)
     # LlamaFlashAttention2 attention does not support output_attentions
     if "padding_mask" in kwargs:
         warnings.warn(
@@ -1236,7 +1240,7 @@ def llama_attn_forward_StreamingLLM(
 ) -> Tuple[torch.Tensor, Optional[torch.Tensor], Optional[Tuple[torch.Tensor]]]:
     bsz, q_len, _ = hidden_states.size()
 
-    init_StreamingLLM(self, num_hidden_layers=self.config.num_hidden_layers)
+    init_StreamingLLM(self)
 
     if self.config.pretraining_tp > 1:
         key_value_slicing = (self.num_key_value_heads * self.head_dim) // self.config.pretraining_tp
@@ -1299,10 +1303,7 @@ def llama_attn_forward_StreamingLLM(
     if past_key_value is not None:
         # sin and cos are specific to RoPE models; cache_position needed for the static cache
         cache_kwargs = {"sin": sin, "cos": cos, "cache_position": cache_position}
-        
-        # 生成第一个token时，整个输入prompt，而且past_key_value.get_usable_length(*)=0，
-        # 所以key_states.shape[-2] == kv_seq_len等价于prefill阶段
-        
+
         if key_states.shape[-2] == kv_seq_len:
             self.kv_seq_len = kv_seq_len
             key_states_compress, value_states_compress = self.kv_cluster.update_kv(key_states, query_states, value_states, attention_mask, self.num_key_value_groups)
@@ -1380,7 +1381,7 @@ def llama_sdpa_attn_forward_StreamingLLM(
             position_embeddings=position_embeddings, # Add to fix 4.44.2
         )
 
-    init_StreamingLLM(self, num_hidden_layers=self.config.num_hidden_layers)
+    init_StreamingLLM(self)
 
     bsz, q_len, _ = hidden_states.size()
 
@@ -1483,7 +1484,7 @@ def llama_flash_attn2_forward_StreamingLLM(
     **kwargs,
 ) -> Tuple[torch.Tensor, Optional[torch.Tensor], Optional[Tuple[torch.Tensor]]]:
     # [SnapKV] register kv_cluster
-    init_StreamingLLM(self, num_hidden_layers=self.config.num_hidden_layers)
+    init_StreamingLLM(self)
     # LlamaFlashAttention2 attention does not support output_attentions
     if "padding_mask" in kwargs:
         warnings.warn(
@@ -1611,7 +1612,7 @@ def llama_attn_forward_SnapKV(
 ) -> Tuple[torch.Tensor, Optional[torch.Tensor], Optional[Tuple[torch.Tensor]]]:
     bsz, q_len, _ = hidden_states.size()
 
-    init_snapkv(self, num_hidden_layers=self.config.num_hidden_layers)
+    init_snapkv(self)
 
     if self.config.pretraining_tp > 1:
         key_value_slicing = (self.num_key_value_heads * self.head_dim) // self.config.pretraining_tp
@@ -1752,7 +1753,7 @@ def llama_sdpa_attn_forward_SnapKV(
             position_embeddings=position_embeddings, # Add to fix 4.44.2
         )
 
-    init_snapkv(self, num_hidden_layers=self.config.num_hidden_layers)
+    init_snapkv(self)
 
     bsz, q_len, _ = hidden_states.size()
 
@@ -1856,7 +1857,7 @@ def llama_flash_attn2_forward_SnapKV(
     **kwargs,
 ) -> Tuple[torch.Tensor, Optional[torch.Tensor], Optional[Tuple[torch.Tensor]]]:
     # [SnapKV] register kv_cluster
-    init_snapkv(self, num_hidden_layers=self.config.num_hidden_layers)
+    init_snapkv(self)
     # LlamaFlashAttention2 attention does not support output_attentions
     if "padding_mask" in kwargs:
         warnings.warn(
@@ -1968,133 +1969,6 @@ def llama_flash_attn2_forward_SnapKV(
         attn_weights = None
 
     return attn_output, attn_weights, past_key_value
-
-def llama_attn_forward_Quest(
-    self,
-    hidden_states: torch.Tensor,
-    attention_mask: Optional[torch.Tensor] = None,
-    position_ids: Optional[torch.LongTensor] = None,
-    past_key_value: Optional[Cache] = None,
-    output_attentions: bool = False,
-    use_cache: bool = False,
-    cache_position: Optional[torch.LongTensor] = None,
-    position_embeddings: Optional[Tuple[torch.Tensor, torch.Tensor]] = None,  # will become mandatory in v4.45
-    **kwargs,
-) -> Tuple[torch.Tensor, Optional[torch.Tensor], Optional[Tuple[torch.Tensor]]]:
-    bsz, q_len, _ = hidden_states.size()
-
-    init_Quest(self, num_hidden_layers=self.config.num_hidden_layers)
-
-    if self.config.pretraining_tp > 1:
-        key_value_slicing = (self.num_key_value_heads * self.head_dim) // self.config.pretraining_tp
-        query_slices = self.q_proj.weight.split(
-            (self.num_heads * self.head_dim) // self.config.pretraining_tp, dim=0
-        )
-        key_slices = self.k_proj.weight.split(key_value_slicing, dim=0)
-        value_slices = self.v_proj.weight.split(key_value_slicing, dim=0)
-
-        query_states = [F.linear(hidden_states, query_slices[i]) for i in range(self.config.pretraining_tp)]
-        query_states = torch.cat(query_states, dim=-1)
-
-        key_states = [F.linear(hidden_states, key_slices[i]) for i in range(self.config.pretraining_tp)]
-        key_states = torch.cat(key_states, dim=-1)
-
-        value_states = [F.linear(hidden_states, value_slices[i]) for i in range(self.config.pretraining_tp)]
-        value_states = torch.cat(value_states, dim=-1)
-
-    else:
-        query_states = self.q_proj(hidden_states)
-        key_states = self.k_proj(hidden_states)
-        value_states = self.v_proj(hidden_states)
-
-    query_states = query_states.view(bsz, q_len, self.num_heads, self.head_dim).transpose(1, 2)
-    key_states = key_states.view(bsz, q_len, self.num_key_value_heads, self.head_dim).transpose(1, 2)
-    value_states = value_states.view(bsz, q_len, self.num_key_value_heads, self.head_dim).transpose(1, 2)
-
-    kv_seq_len = key_states.shape[-2]
-
-    if past_key_value is not None:
-        if self.layer_idx is None:
-            raise ValueError(
-                f"The cache structure has changed since version v4.36. If you are using {self.__class__.__name__} "
-                "for auto-regressive decoding with k/v caching, please make sure to initialize the attention class "
-                "with a layer index."
-            )
-        if hasattr(self, "kv_seq_len"): 
-            if self.kv_seq_len != 0:
-                kv_seq_len += self.kv_seq_len
-            else:
-                kv_seq_len += past_key_value.get_usable_length(kv_seq_len, self.layer_idx)
-        else:
-            kv_seq_len += past_key_value.get_usable_length(kv_seq_len, self.layer_idx)
-    
-    if position_embeddings is None: # Add to fix 4.44.2
-        logger.warning_once(
-            "The attention layers in this model are transitioning from computing the RoPE embeddings internally "
-            "through `position_ids` (2D tensor with the indexes of the tokens), to using externally computed "
-            "`position_embeddings` (Tuple of tensors, containing cos and sin). In v4.45 `position_ids` will be "
-            "removed and `position_embeddings` will be mandatory."
-        )
-        cos, sin = self.rotary_emb(value_states, position_ids)
-    else:
-        cos, sin = position_embeddings
-    
-    query_states, key_states = apply_rotary_pos_emb(query_states, key_states, cos, sin)
-    key_states = repeat_kv(key_states, self.num_key_value_groups)
-    value_states = repeat_kv(value_states, self.num_key_value_groups)
-
-    if past_key_value is not None:
-        # sin and cos are specific to RoPE models; cache_position needed for the static cache
-        cache_kwargs = {"sin": sin, "cos": cos, "cache_position": cache_position}
-
-        if key_states.shape[-2] == kv_seq_len:
-            self.kv_seq_len = kv_seq_len
-            key_states_compress, value_states_compress = self.kv_cluster.update_kv(key_states, query_states, value_states, attention_mask, self.num_key_value_groups)
-            past_key_value.update(key_states_compress, value_states_compress, self.layer_idx, cache_kwargs)
-        else:
-            self.kv_seq_len += q_len
-            key_states, value_states = past_key_value.update(key_states, value_states, self.layer_idx, cache_kwargs)
-            ##### Use kv_cluster to update kv in decoding phase #####
-            history_key_states, history_value_states = past_key_value[self.layer_idx]
-            key_states_compress, value_states_compress, key_store, value_store = self.kv_cluster.update_kv_in_decoding(history_key_states, query_states, history_value_states, attention_mask, position_ids, self.layer_idx, self.num_key_value_groups)
-            past_key_value.key_cache[self.layer_idx] = key_store
-            past_key_value.value_cache[self.layer_idx] = value_store
-    
-    attn_weights = torch.matmul(query_states, key_states.transpose(2, 3)) / math.sqrt(self.head_dim)
-
-    if attention_mask is not None:  # no matter the length, we just slice it
-        causal_mask = attention_mask[:, :, :, : key_states.shape[-2]]
-        attn_weights = attn_weights + causal_mask
-    
-    # upcast attention to fp32
-    attn_weights = nn.functional.softmax(attn_weights, dim=-1, dtype=torch.float32).to(query_states.dtype)
-    attn_weights = nn.functional.dropout(attn_weights, p=self.attention_dropout, training=self.training)
-    attn_output = torch.matmul(attn_weights, value_states)
-
-    if attn_output.size() != (bsz, self.num_heads, q_len, self.head_dim):
-        raise ValueError(
-            f"`attn_output` should be of size {(bsz, self.num_heads, q_len, self.head_dim)}, but is"
-            f" {attn_output.size()}"
-        )
-    
-    attn_output = attn_output.transpose(1, 2).contiguous()
-
-    attn_output = attn_output.reshape(bsz, q_len, self.hidden_size)
-
-    if self.config.pretraining_tp > 1:
-        attn_output = attn_output.split(self.hidden_size // self.config.pretraining_tp, dim=2)
-        o_proj_slices = self.o_proj.weight.split(self.hidden_size // self.config.pretraining_tp, dim=1)
-        attn_output = sum([F.linear(attn_output[i], o_proj_slices[i]) for i in range(self.config.pretraining_tp)])
-    else:
-        attn_output = self.o_proj(attn_output)
-
-    if not output_attentions:
-        attn_weights = None
-
-    return attn_output, attn_weights, past_key_value
-
-
-    
 
 from transformers.models.llama.modeling_llama import _prepare_4d_causal_attention_mask_with_cache_position, StaticCache # Add to fix 4.44.2
 
